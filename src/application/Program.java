67c351639192be8fe701b4f717bda3b6c02d321c
mig-ramos <mig.ramos10@gmail.com>
2020-10-28 23:11:07 -0300

    Departament entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Departament;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departament obj = new Departament(1, "Book");
+		System.out.println(obj);
+
+	}
+
+}
